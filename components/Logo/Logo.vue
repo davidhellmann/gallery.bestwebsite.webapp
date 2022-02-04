d9f32132393d6ede41931495c330151389341710
davidhellmann <david@hellmann.io>
2022-02-04 11:52:26 +0100

    💄 style: logo

--- components/Logo/Logo.vue
+++ components/Logo/Logo.vue
@@ -1,15 +1,24 @@
 <template>
   <Link
     :class="[
-      'flex flex-row items-center font-medium text-gray-900 group dark:text-gray-100 text-uiBase',
+      'flex flex-row items-center font-medium text-gray-900 group dark:text-gray-100 w-52 h-16 text-uiBase relative',
       tw,
     ]"
     to="/"
   >
     <span
-      class="w-3 h-3 group-hover:w-5 group-hover:h-5 transition transition-all transform -translate-y-px mr-2 rounded-xl bg-blue-500 dark:bg-gray-400 inline-block"
+      class="w-3 h-3 group-hover:w-2 group-hover:h-2 absolute left-0 transition transition-all transform -translate-y-px rounded-xl bg-blue-500 dark:bg-gray-400 group-hover:bg-dot-red inline-block"
     ></span>
-    Best Website Gallery
+    <span
+      class="w-3 h-3 group-hover:w-2 group-hover:h-2 absolute left-0 group-hover:translate-x-3 opacity-0 group-hover:opacity-100 transition transition-all transform -translate-y-px rounded-xl bg-blue-500 dark:bg-gray-400 group-hover:bg-dot-yellow inline-block"
+    ></span>
+    <span
+      class="w-3 h-3 group-hover:w-2 group-hover:h-2 absolute left-0 group-hover:translate-x-6 opacity-0 group-hover:opacity-100 transition transition-all transform -translate-y-px rounded-xl bg-blue-500 dark:bg-gray-400 group-hover:bg-dot-green inline-block"
+    ></span>
+    <span
+      class="absolute left-0 w-44 transition transform translate-x-6 group-hover:translate-x-12"
+      >Best Website Gallery</span
+    >
   </Link>
 </template>
 
